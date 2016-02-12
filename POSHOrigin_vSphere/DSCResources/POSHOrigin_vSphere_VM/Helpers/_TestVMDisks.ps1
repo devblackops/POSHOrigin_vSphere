7831ac80c75028751f991b12aef7c6ae95c2181c
Brandon Olin <brandon@devblackops.io>
2016-02-12 07:12:00 -0800

    Add logic to begin validate SCSI parameters

--- POSHOrigin_vSphere/DSCResources/POSHOrigin_vSphere_VM/Helpers/_TestVMDisks.ps1
+++ POSHOrigin_vSphere/DSCResources/POSHOrigin_vSphere_VM/Helpers/_TestVMDisks.ps1
@@ -12,6 +12,10 @@ function _TestVMDisks {
 
     begin {
         Write-Debug -Message '_TestVMDisks() starting'
+        $vmDiskStorageFormat = [string]::Empty
+        $diskStorageFormat = [string]::Empty
+
+        $diskCount = $diskExist = $diskSCSI = $diskSize = $diskType = $diskFormat = $true
     }
 
     process {
@@ -23,9 +27,11 @@ function _TestVMDisks {
 
             if ( @($configDisks).Count -ne @($vmDisks).Count) {
                 Write-Verbose -Message 'Disk count does not match configuration'
-                $pass = $false
-                return $pass
+                $diskCount = $false
+                #return $pass
             }
+
+            $scsiControllers = $vm | Get-SCSIController -Verbose:$false 
 
             foreach ($disk in $configDisks) {
                 Write-Debug -Message "Validating VM disk [$($disk.Name)]"
@@ -33,7 +39,34 @@ function _TestVMDisks {
                 $vmDisk = $vmDisks | Where-Object {$_.Name.ToLower() -eq $disk.Name.ToLower() }
                 if ($null -eq $vmDisk) {
                     Write-Verbose -Message "Disk [$($disk.Name)] does not exist on VM"
-                    return $false
+                    $diskExist = $false
+                    #return $false
+                }
+
+                # TODO
+                # Validate VM disk is on correct SCSI controller / bus number
+                if ($disk.SCSI) {
+
+                    # The SCSI Id the VM disk 'should' be on
+                    $desiredSCSIControllerNumber = $disk.SCSI.Id.Split(':')[0]
+                    $desiredBusNumber = $disk.SCSI.Id.Split(':')[1]
+
+                    $actualSCSIController = $vmDisk | Get-SCSIController -Verbose:$false
+                    $actualSCSIControllerNum = $actualSCSIController.Name.Split(' ')[2]
+                    $actualSCSIBusNumber = ($vm.ExtensionData.Config.Hardware.Device |
+                        where { $_.Key -eq $vmDisk.ExtensionData.Key} | Select -First 1).UnitNumber
+
+                    Write-Verbose -Message "[$($vmDisk.Name)] is SCSI [$actualSCSIControllerNum : $actualSCSIBusNumber]"
+
+                    # Does this VM disk match the desired SCSI ID?
+                    if (($actualSCSIControllerNum -ne $desiredSCSIControllerNumber) -or
+                        ($actualSCSIBusNumber -ne $desiredBusNumber)) {
+                        Write-Verbose -Message "Disk [$($vmDisk.Name)] SCSI ID does not match [$actualSCSIControllerNum`:$actualSCSIBusNumber <> $desiredSCSIControllerNumber`:$desiredBusNumber]"
+                        $diskSCSI = $false
+                        #return $false
+                    }
+
+
                 }
 
                 $vmDiskCap = [system.math]::round($vmDisk.CapacityGB, 0)
@@ -44,29 +77,31 @@ function _TestVMDisks {
                         Write-Warning -Message "The current disk size [$vmDiskCap GB] is greater than the desired disk size [$($disk.SizeGB) GB]. Can not shrink VM disks"
                     } else {
                         Write-Verbose -Message "Disk [$($disk.Name)] does not match configured size"
-                        return $false
+                        $diskSize = $false
+                        #return $false
                     }
                 }
 
-                $vmDiskStorageFormat = ''
                 if ($null -ne $vmDisk.StorageFormat) {
                     $vmDiskStorageFormat = $vmDisk.StorageFormat
                 }
-                $diskStorageFormat = ''
                 if ($null -ne $disk.Format) {
                     $diskStorageFormat = $disk.Format
                 }
                 if ($vmDiskStorageFormat.ToString().ToLower() -ne $diskStorageFormat.ToLower()) {
-                    Write-Verbose -Message "Disk [$($disk.Name)] does not match configured format"
-                    return $false
+                    Write-Verbose -Message "Disk [$($disk.Name)] storage format [$($vmDiskStorageFormat.ToString().ToLower()) <> @($diskStorageFormat.ToLower()) )]"
+                    $diskFormat = $false
+                    #return $false
                 }
-
                 if ($vmDisk.DiskType.ToString().ToLower() -ne $disk.Type.ToLower()) {
-                    Write-Verbose -Message "Disk [$($disk.Name)] does not match configured type"
-                    return $false
+                    Write-Verbose -Message "Disk [$($disk.Name)] type [$($vmDisk.DiskType.ToString().ToLower()) <> $($disk.Type.ToLower())]"
+                    $diskType = $false
+                    #return $false
                 }
             }
-            return $true
+
+            return ($diskCount -and $diskExist -and $diskSCSI -and $diskSize, $diskType, $diskFormat)
+            #return $true
         } catch {
             Write-Error -Message 'There was a problem testing the disks.'
             Write-Error -Message "$($_.InvocationInfo.ScriptName)($($_.InvocationInfo.ScriptLineNumber)): $($_.InvocationInfo.Line)"
