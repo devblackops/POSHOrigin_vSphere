1b36fdaacbcbb2d5d9679a299ba754a3cba72f41
brandon@devblackops.io <brandon@devblackops.io>
2016-06-03 21:41:10 -0700

    Test for tags correctly

--- POSHOrigin_vSphere/DSCResources/POSHOrigin_vSphere_VM/Helpers/_testTags.ps1
+++ POSHOrigin_vSphere/DSCResources/POSHOrigin_vSphere_VM/Helpers/_testTags.ps1
@@ -16,19 +16,20 @@ function _TestTags {
     $desiredTags = $Tags | ConvertFrom-Json
     $tagAssignments = $vm | Get-TagAssignment -Verbose:$false
         
-    # Verify that each desired tag in configuration is applied in vCenter
-    # Apply if necessary
+    # Verify that each desired tag in configuration is applied in vCenter. Apply if necessary
     foreach ($desiredTag in $desiredTags) {
         $match = $tagAssignments | Where-Object {($_.Tag.Category.Name -eq $desiredTag.Category) -and ($_.Tag.Name -eq $desiredTag.Name)}
         if (-not $match) {
+            Write-Verbose -Message "Tag [$($desiredTag.Category)/$($desiredTag.Name)] not set"
             $result = $false
         }
     }
     
     # Remove any tag assignments in vCenter that are NOT in the desired tag list
     foreach ($tagAssignment in $tagAssignments) {
-        $match = $desiredTags | Where-Object {($_.Category -eq $tagAssistnment.Tag.Category.Name) -and ($_.Name -eq $tagAssistnment.Tag.Name)}
+        $match = $desiredTags | Where-Object {($_.Category -eq $tagAssignment.Tag.Category.Name) -and ($_.Name -eq $tagAssignment.Tag.Name)}
         if (-not $match ) {
+            Write-Verbose -Message "Tag [$($tagAssignment.Tag.Category.Name)/$($tagAssignment.Tag.Name)] should be removed"
             $result = $false
         }
     }
