7b1a66ef4c46725e28ddc656a17e80ca6f0bf484
Brandon Olin <brandon@devblackops.io>
2016-07-31 14:35:49 -0700

    Rename PSDeploy script and use BuildHelpers

--- psgallery.psdeploy.ps1
+++
@@ -1,9 +0,0 @@
-Deploy POSHOrigin_vSphere {
-    By PSGalleryModule ToPSGallery {
-        FromSource '.\POSHOrigin_vSphere'
-        To 'PSGallery'
-        WithOptions @{
-            ApiKey = $env:PSGALLERY_API_KEY
-        }
-    }
-}
