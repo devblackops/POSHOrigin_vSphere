4ba9ca650379220b7b42f96b1c0b83cc471e0ad9
Brandon Olin <bolin@columbia.com>
2016-12-01 12:09:15 -0800

    Use IP address

--- POSHOrigin_vSphere/DSCResources/POSHOrigin_vSphere_VM/Helpers/_NewCIMSession.ps1
+++ POSHOrigin_vSphere/DSCResources/POSHOrigin_vSphere_VM/Helpers/_NewCIMSession.ps1
@@ -3,62 +3,54 @@ function _NewCIMSession {
     [cmdletbinding()]
     param(
         [Parameter(Mandatory)]
-        [ValidateNotNull()]
-        $VM,
+        [string]$IPAddress,
 
         [Parameter(Mandatory)]
         [pscredential]$Credential
     )
 
-    $ip = _GetGuestVMIPAddress -VM $VM
+    # Establish a CIM session
+    $sessionParams = @{
+        ComputerName = $IPAddress
+        Credential = $Credential
+        Verbose = $false
+    }
+    $cim = New-CimSession @sessionParams
 
-    if ($ip) {
+    if ($cim) {
 
-        # Establish a CIM session
-        $sessionParams = @{
-            ComputerName = $ComputerName
-            Credential = $Credential
+        $ciParams = @{
+            ClassName = 'Win32_OperatingSystem'
             Verbose = $false
+            ErrorAction = 'SilentlyContinue'
         }
-        $cim = New-CimSession @sessionParams
+        # Verify this session works by executing a query.
+        $os = Get-CimInstance -CimSession $cim @ciParams
 
-        if ($cim) {
-
-            $ciParams = @{
-                ClassName = 'Win32_OperatingSystem'
-                Verbose = $false
-                ErrorAction = 'SilentlyContinue'
-            }
-            # Verify this session works by executing a query.
-            $os = Get-CimInstance -CimSession $cim @ciParams
-
-            if ($os) {
-                # This CIM session works
-                Write-Debug -Message 'Successfully established CIM session'
-                return $cim
-            } else {
-                Write-Debug -Message 'Unable to establish CIM session. Trying again using DCOM'
-                # On Windows 2008, we may have to use the DCOM session option
-                $cim | Remove-CimSession -Verbose:$false
-                $sessionParams.SessionOption = New-CimSessionOption -Protocol Dcom
-                $cim = New-CimSession @sessionParams
-                if ($cim) {
-                    $os = Get-CimInstance -CimSession $cim @ciParams
-                    if ($os) {
-                        Write-Debug -Message 'Successfully established CIM session using DCOM option'
-                        return $cim
-                    } else {
-                        $cim | Remove-CimSession -Verbose:$false
-                        Write-Error -Message 'Unable to establish CIM session with DCOM option'
-                    }
+        if ($os) {
+            # This CIM session works
+            Write-Debug -Message 'Successfully established CIM session'
+            return $cim
+        } else {
+            Write-Debug -Message 'Unable to establish CIM session. Trying again using DCOM'
+            # On Windows 2008, we may have to use the DCOM session option
+            $cim | Remove-CimSession -Verbose:$false
+            $sessionParams.SessionOption = New-CimSessionOption -Protocol Dcom
+            $cim = New-CimSession @sessionParams
+            if ($cim) {
+                $os = Get-CimInstance -CimSession $cim @ciParams
+                if ($os) {
+                    Write-Debug -Message 'Successfully established CIM session using DCOM option'
+                    return $cim
                 } else {
+                    $cim | Remove-CimSession -Verbose:$false
                     Write-Error -Message 'Unable to establish CIM session with DCOM option'
                 }
+            } else {
+                Write-Error -Message 'Unable to establish CIM session with DCOM option'
             }
-        } else {
-            Write-Error -Message 'Unable to establish CIM session'
         }
     } else {
-        Write-Error -Message 'Unable to retrieve guest VM IP address. Can not establish CIM session.'
+        Write-Error -Message 'Unable to establish CIM session'
     }
 }
